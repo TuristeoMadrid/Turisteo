bb267736e8268260af7b171d7866baf5518d86f3
Mapno <mariorvinas@gmail.com>
2018-09-25 12:47:01 +0200

    profile page

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const router  = express.Router();
-const Route = require('../models/Routes');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  Route.find()  
-  .then(routes => {
-    res.render('index', {
-      user: req.user,
-      routes,
-      routesStr: JSON.stringify(routes)})
-  })
+    res.render('index', {user: req.user})
 });
 
 
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router  = express.Router();
 const { ensureLoggedIn, ensureLoggedOut } = require("connect-ensure-login");
+const Route = require('../models/Routes');
+
 
 router.get('/profile', ensureLoggedIn(), (req,res) => {
-  res.render('profile', {user: req.user});
+  Route.find()  
+  .then(routes => {
+    res.render('profile', {
+      user: req.user,
+      routes,
+      routesStr: JSON.stringify(routes)});
+    });
 });
 
 module.exports = router;
