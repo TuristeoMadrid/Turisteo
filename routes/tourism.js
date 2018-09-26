023df57607218fa654c763b0430829f2458fae85
Evenstarw1 <lauracorrallopez@gmail.com>
2018-09-26 17:32:45 +0200

    Cambiando estilos

--- routes/tourism.js
+++ routes/tourism.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router  = express.Router();
 const { ensureLoggedIn, ensureLoggedOut } = require("connect-ensure-login");
 const Route = require('../models/Routes');
+const Place = require('../models/PlaceOfInterest');
 
 router.get('/', ensureLoggedIn(), (req,res) => {
+  Place.find()
   Route.find()  
   .then(routes => {
     res.render('tourism/visit', {
