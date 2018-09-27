b0d9c120a6976e79a22020565a743042576c903e
Mapno <mariorvinas@gmail.com>
2018-09-27 14:37:15 +0200

    fix

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const flash = require("connect-flash");
     
 
 mongoose
-  .connect('mongodb://localhost/turisteo', {useNewUrlParser: true})
+  .connect(process.env.DBURL, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -45,7 +45,7 @@ app.use(require('node-sass-middleware')({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(favicon(path.join(__dirname, 'public', 'images', 'ico.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'ico.ico')));
 
 
 hbs.registerHelper('ifUndefined', (value, options) => {
