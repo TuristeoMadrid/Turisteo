17bb46bed3ebc633b6626a93c9bca1998d33d7df
Mapno <mariorvinas@gmail.com>
2018-09-27 13:07:31 +0200

    homepage and profile

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router  = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
     res.render('index', {user: req.user})
 });
-
 
 module.exports = router;
