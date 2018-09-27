2e32ef5b21b49504fa951f8d9eae85bf5302657e
Mapno <mariorvinas@gmail.com>
2018-09-27 21:50:36 +0200

    update users page

--- public/javascripts/update.js
+++ public/javascripts/update.js
@@ -1,1 +1,19 @@
-let updateBtns = document.getElementsByClassName('deleteBtn')
+const updateBtns = document.getElementsByClassName('updateBtn');
+const updatePrevBtn = document.getElementsByClassName('updatePrevBtn');
+const changeToForm = ele => {
+  const pArr = document.getElementsByClassName(`${ele.id}p`);
+  const inputArr = document.getElementsByClassName(`${ele.id}input`);
+  const bArr = document.getElementsByClassName(`${ele.id}b`);
+  for(let i = 0; i < pArr.length; i++){
+    inputArr[i].removeAttribute('hidden');
+    pArr[i].setAttribute('hidden', 'hidden');
+    ele.setAttribute('hidden', 'hidden');
+    ele.nextSibling.nextSibling.removeAttribute('hidden');
+  };
+};
+const eventAdd = arr => {
+  for(let i = 0; i < arr.length; i++) arr[i].addEventListener('click', () => { changeToForm(arr[i])});
+}
+document.addEventListener('DOMContentLoaded', () => {
+  eventAdd(updatePrevBtn);
+}, false);
