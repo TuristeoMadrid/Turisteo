3875c5906fbfee66b3053a933113f34ecb2f2211
Mapno <mariorvinas@gmail.com>
2018-09-27 16:28:15 +0200

    description display

--- public/javascripts/popover.js
+++ public/javascripts/popover.js
@@ -1,12 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const btns = document.getElementsByClassName('btn');
-  const popover = btn => {
-    $(btn).popover({
-      container: 'body'
-    })
-  }
-  for (let i = 0; i < btns.length; i++) {
-    console.log(btns[i])
-    btns[i].addEventListener('mouseover', () => {popover(btns[i])})
-  }
+  $(function() {
+    $(".btn").tooltip({
+        show: {
+        effect: "slideDown",
+        delay: 300
+        }
+    });
+    $( function() {
+      // run the currently selected effect
+      function runEffect(id) {
+        const options = {};
+        // Run the effect
+        $( `#${id}1` ).show( 'fade', options, 200, callback );
+      };
+   
+      //callback function to bring a hidden box back
+      function callback() {
+        setTimeout(function() {
+          $( ".effect:visible" ).removeAttr( "style" ).fadeOut();
+        }, 1000 );
+      };
+   
+      // Set effect from select menu value
+      $( ".btn" ).on( "mouseover", function(e) {
+        runEffect(e.target.id);
+      });
+   
+      $( ".effect" ).hide();
+    } );
+  
+});
 }, false);
